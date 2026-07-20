bd2fb52e282095ace4657e33d35f17dc077e36ef
faisalalsalih <faisal.salih.hayat768@gmail.com>
2026-07-20 10:24:29 +0500

    Add shadcnUI

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
+
 
 const Home = () => {
   return (
     <>
-      
+      <div className="mt-9 flex flex-col gap-9">
+
+        <section className="flex flex-col gap-5">
+          <h1 className="text-20 font-bold text-charcoal">Trending Podcasts</h1>
+        </section>
+
+      </div>
     </>
   )
 }
