b785d52781452989068b9bb4db54c73c6699df0f
faisalalsalih <faisal.salih.hayat768@gmail.com>
2026-07-22 14:53:07 +0500

    Solve the error of connecting the frontend with the backend of convex

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,36 +1,50 @@
+"use client";
+
 import { podcastData } from '@/constants'
 import React from 'react'
 import PodcastCard from '@/components/shared/PodcastCard'
-
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 
 const Home = () => {
+
+  const tasks = useQuery(api.tasks.get);
+
   return (
     <>
       <div className="mt-9 flex flex-col gap-9">
-        
+
         <section className="flex flex-col gap-5">
 
           <h1 className="text-20 font-bold text-charcoal">Trending Podcasts</h1>
 
-        <div className="podcast_grid">
-          {
-            podcastData.map((podcast) => (
+          <div className="flex min-h-screen flex-col items-center justify-between p-24 text-white">
 
-              <PodcastCard
-              key={podcast.id}
-              title={podcast.title}
-              description={podcast.description}
-              podcastId={podcast.id}
-              imgUrl={podcast.imgURL}
-              />
+            {tasks?.map(({ _id, text }) => (
+              <div key={_id} className="text-charcoal">{text}</div>
+            ))}
+            
+          </div>
 
-            ))
-          }
-        </div>
+          <div className="podcast_grid">
+            {
+              podcastData.map((podcast) => (
+
+                <PodcastCard
+                  key={podcast.id}
+                  title={podcast.title}
+                  description={podcast.description}
+                  podcastId={podcast.id}
+                  imgUrl={podcast.imgURL}
+                />
+
+              ))
+            }
+          </div>
 
         </section>
-        
+
       </div>
     </>
   )
